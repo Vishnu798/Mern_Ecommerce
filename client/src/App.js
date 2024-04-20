bd52d3f430c05eb53bd1ee1689fb27167607d1d0
vishnu798 <vishnukhandelwal14@gmail.com>
2024-04-20 23:28:30 +0530

    carousel added

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,10 @@ import {
 import HomePage from './pages/home_page';
 import AdminUploadProduct from './pages/admin_upload_product';
 import AdminLayout from './components/admin_page_layout';
-import Carousel from './components/carousel';
 import UserPage from './pages/user_page';
 function App() {
   
-  const images = [
-    'https://via.placeholder.com/800x400/ff5733/fff',
-    'https://via.placeholder.com/800x400/33ff57/fff',
-    'https://via.placeholder.com/800x400/5733ff/fff',
-  ];
+
   return (
     <>
     <BrowserRouter>
