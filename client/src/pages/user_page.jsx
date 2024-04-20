d4efdbce090136ab67b4eba7d998f95ee7adbcf5
vishnu798 <vishnukhandelwal14@gmail.com>
2024-04-20 23:32:34 +0530

    carousel added

--- client/src/pages/user_page.jsx
+++ client/src/pages/user_page.jsx
@@ -7,7 +7,7 @@ const UserPage = () => {
       <div className='d-flex top-image flex-column align-items-center justify-content-center '>
         
           <h4 className='move' > Make Shopping Easy And Fashionable</h4>
-        <img className='top-image' src='https://images.ctfassets.net/wowgx05xsdrr/5YszVlLodSONRRVN9O6CE6/107a1a991daad1e92dda43dac5de03e0/shopping-cart-software-Header.png'/>
+        <img className='top-image' src='https://images.ctfassets.net/wowgx05xsdrr/5YszVlLodSONRRVN9O6CE6/107a1a991daad1e92dda43dac5de03e0/shopping-cart-software-Header.png' alt='decoration'/>
         </div>
           <Carousels/>
     </>
